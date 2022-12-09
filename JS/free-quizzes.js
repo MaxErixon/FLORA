70e19496dfdbb071250dbb7c495f8fe42d931cf5
lovisaericsson <lovisa.ericsson01@gmail.com>
2022-12-09 14:52:55 +0100

    Started with free-quiz

---
+++ JS/free-quizzes.js
@@ -0,0 +1,55 @@
+"use-strict";
+
+const category = [
+    {   id: 1,
+        title: "Arts & Literature",
+        icon: "../Images/Categories/arts.jpg",
+        information: "This category contains questions and answers about literature. Questions about authors, books, poets, philosophers and literary figures."
+    },
+    {   id: 2,
+        title: "Film & Tv",
+        icon: "../Images/Categories/movies.jpg",
+        information: "This category contains questions and answers about movies. Questions about movie titles, actors, directors, scripts and movie roles."
+    },
+    {   id: 3,
+        title: "Food & Drink",
+        icon: "../Images/Categories/food.jpg",
+        information: "This category contains questions about food, recipes, international cuisine, wine, beer and other drinks."
+    },
+    {   id: 4,
+        title: "General Knowledge",
+        icon: "../Images/Categories/knowledge.jpg",
+        information: "Quiz general education contains mixed questions and answers in general knowledge. Questions about geography, history, literature, music, film, science, sports and much more."
+    },
+    {   id: 5,
+        title: "Geography",
+        icon: "../Images/Categories/geography.jpg",
+        information: "This category contains questions and answers about geography. Questions about countries, cities, mountains, seas, lakes and much more."
+    },
+    {   id: 6,
+        title: "History",
+        icon: "../Images/Categories/history.jpg",
+        information: "This category contains questions and answers about history. Questions about historical events, periods, places and people."
+    },
+    {   id: 7,
+        title: "Music",
+        icon: "../Images/Categories/music.jpg",
+        information: "This category contains questions and answers about music titles, artists, composers and styles."
+    },
+    {   id: 8,
+        title: "Science",
+        icon: "../Images/Categories/science.jpg",
+        information: "This category contains questions and answers about science. Questions about mathematics, chemistry, astronomy and physics."
+    },
+    {   id: 9,
+        title: "Society & Culture",
+        icon: "../Images/Categories/society.jpg",
+        information: "This category contains questions and answers about society and culture"
+    },
+    {   id: 10,
+        title: "Sport & Leisure",
+        icon: "../Images/Categories/sport.jpg",
+        information: "This category contains questions and answers about sports and leisure. Questions about leisure, games, sports, athletes, sporting events and individual sporting achievements.."
+    },
+
+]
