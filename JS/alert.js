d6acdada38d8dbddfffeb03495de007c7b347306
melissasjostedt <melissa.sjostedt@outlook.com>
2022-12-12 14:47:12 +0100

    update

---
+++ JS/alert.js
@@ -0,0 +1,12 @@
+"use strict"
+
+function DeleteAccount() {
+confirm("Are you sure you want to delete your account?");
+if(document.getElementById(""))
+}
+
+
+function Saved() {
+    confirm("Are you sure you want to delete your account?");
+    if(document.getElementById(""))
+    }
