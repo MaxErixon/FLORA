692b487500f6ffb9752909d48483c5a047a8b11d
melissasjostedt <melissa.sjostedt@outlook.com>
2022-12-13 18:48:03 +0100

    #63
    updated

--- JS/alert.js
+++ JS/alert.js
@@ -1,18 +1,17 @@
 "use strict"
 
 function DeleteAccount() {
-  let answer = confirm('Are you sure you want to delete your account?');
-    console.log(answer);
-    if (answer === true){
-        let newPopUp = alert('Your account has been deleted!');
-        document.createElement("button").innerHTML;
-        location.href("home-free.html");
-    }
+    let answer = confirm('Are you sure you want to delete your account?');
+      console.log(answer);
+      if (answer === true){
+          let newPopUp = alert('Your account has been deleted!');
+          document.createElement("button").innerHTML;
+          location.href("home-free.html");
+      }
+     
    
- 
-   
-}
-
+     
+  }
 function SavedEditings() {
     confirm("Are you sure you want to save your editings?");
    
