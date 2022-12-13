eb3ae3bb17da847ee5732cc80608d0b482d11160
melissasjostedt <melissa.sjostedt@outlook.com>
2022-12-13 18:23:08 +0100

    NOT finished
    #59 updated version

--- JS/alert.js
+++ JS/alert.js
@@ -1,12 +1,21 @@
 "use strict"
 
 function DeleteAccount() {
-confirm("Are you sure you want to delete your account?");
-if(document.getElementById(""))
+  let answer = confirm('Are you sure you want to delete your account?');
+    console.log(answer);
+    if (answer === true){
+        let newPopUp = alert('Your account has been deleted!');
+        document.createElement("button").innerHTML;
+        location.href("home-free.html");
+    }
+   
+ 
+   
 }
 
+function SavedEditings() {
+    confirm("Are you sure you want to save your editings?");
+   
+}
 
-function Saved() {
-    confirm("Are you sure you want to delete your account?");
-    if(document.getElementById(""))
-    }
+DeleteAccount();
