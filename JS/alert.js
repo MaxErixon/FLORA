e2f93e9b61cb50b314217b5c9e4e4a1cd6816283
melissasjostedt <melissa.sjostedt@outlook.com>
2022-12-14 11:04:34 +0100

    updated
    updated

--- JS/alert.js
+++ JS/alert.js
@@ -1,12 +1,16 @@
 "use strict"
 
 function DeleteAccount() {
-confirm("Are you sure you want to delete your account?");
-if(document.getElementById(""))
+var answer = confirm("Are you sure you want to delete your account?");
+if (answer === true) {
+    console.log(answer);
+    alert('Your account has been deleted!');
+    location.href = "home-free.html";
+}
 }
 
-
-function Saved() {
-    confirm("Are you sure you want to delete your account?");
-    if(document.getElementById(""))
-    }
+function SavedEditings() {
+    location.href = "home-account.html"
+    confirm("Your editings have been saved!");
+   
+}
