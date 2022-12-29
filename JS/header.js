5f9c406a63c98e5d200ae5bf77fd1e34cbc0e6a6
MaxErixon <maxerixon112@gmail.com>
2022-12-29 18:24:46 +0100

    Update- show name + smal fixes

--- JS/header.js
+++ JS/header.js
@@ -2,16 +2,16 @@ document.querySelector("header").innerHTML =
 `<div class="header">
       
       <a href="home-account.html"><img src="../Images/Quiz-logo.png" class = "logo"></a>
-      <h2>${localStorage.getItem("setName")}</h2>
-
+      
       <h3 class="user-text"><a href="user-questions.html">User Quiz</a></h3>
       <h3 class="submit">
-        <a href="SubmitQuestion.html">Submit a question</a>
+      <a href="SubmitQuestion.html">Submit a question</a>
       </h3>
       <h3 class="edit">
-        <a href="userSettings.html">Edit account settings</a>
+      <a href="userSettings.html">Edit account settings</a>
       </h3>
       <button id="icon-user">
+      <h2>${localStorage.getItem("setName")}</h2>
         <img src="../Images/user.jpg" alt="buttonpng" />
       </button>
     </div>`
