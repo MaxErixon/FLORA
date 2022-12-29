2dc657f5987675dd038817e5a116bac3e52baec6
MaxErixon <maxerixon112@gmail.com>
2022-12-29 21:04:15 +0100

    updates

--- JS/header.js
+++ JS/header.js
@@ -3,12 +3,12 @@ document.querySelector("header").innerHTML =
       
       <a href="home-account.html"><img src="../Images/Quiz-logo.png" class = "logo"></a>
       
-      <h3 class="user-text"><a href="user-questions.html">User Quiz</a></h3>
+      <h3 class="user-text"><a href="free-quiz.html">Cateogry quizzes</a></h3>
       <h3 class="submit">
-      <a href="SubmitQuestion.html">Submit a question</a>
+      <a href="user-questions.html">User questions</a>
       </h3>
       <h3 class="edit">
-      <a href="userSettings.html">Edit account settings</a>
+      <a href="SubmitQuestion.html">Submit a question</a>
       </h3>
       <a href = "userSettings.html" id="icon-user">
       <h2>${localStorage.getItem("setName")}</h2>
