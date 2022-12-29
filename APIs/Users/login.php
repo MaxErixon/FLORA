d3e7a4703779a57889a0b7f14683927b02367c9b
melissasjostedt <melissa.sjostedt@outlook.com>
2022-12-29 10:01:22 +0100

    Update login.php
    Update

--- APIs/Users/login.php
+++ APIs/Users/login.php
@@ -5,35 +5,47 @@ require_once "functions.php";
 //Displays errors
 ini_set("display_errors", 1);
 
+
+$method = $_SERVER["REQUEST_METHOD"];
 // If request method is not POST send error message 
-if($request_method !== "POST"){
+if($method !== "POST"){
     $error = ["error" => "Only POST method is allowed"]; 
     sendJSON($error, 400);
-}
+} 
 
-//The r_data must include username and password 
-if(isset($requestData["username"], $requestData["password"])){
-    if(empty($requestData["username"]) or empty($requestData["password"])){
-        $error = ["error" => "Please fill in all of the information."];
-        sendJSON($error, 404);
+// Any page you want to perform session-checks on needs to start with:
+    session_start();
+
+    if($_SESSION["loggedIn"] !== true){
+        echo 'not logged in';
+        header("Location: login.php");
+        exit;
     }
-    //if the requestData is correct, loop through the database of 
-    //users and check that the user exists
-    foreach($users as $user){
-        if($user["username"] == $requestData["username"] and $user["password"] == $requestData["password"]){
-            //Allwo the user to login if it's in the database.
-            sendJSON($user);
+    
+    $_SESSION["loggedIn"] = true;
 
+
+function findUser($username, $password){
+        if($requestData['username'] && $requestData['password'] !== "") {
+            foreach($user as $users){
+                $requestData['id'] = $id;
+                $requestData['username'] = $username;
+                $requestData['password'] = $password;
+                sendJSON($user);
+            }{
+                $error = ["error" => "No user found."];
+                sendJSON($error, 400);
+            }
         }
-    }
-    $error = ["error" => "No user found."];
-    sendJSON($error, 400);
 }
 
+function loginUser(){
+
+
+}
 
 
 ?>
-?> 
 
 
 
