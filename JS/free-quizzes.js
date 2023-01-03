e0983ca59bb315f80312fe544cc42307b6e73d45
MaxErixon <maxerixon112@gmail.com>
2023-01-03 18:35:07 +0100

    Updates

--- JS/free-quizzes.js
+++ JS/free-quizzes.js
@@ -442,7 +442,7 @@ function playQuiz() {
         infoQuiz.appendChild(backToMenu);
 
         backToMenu.addEventListener("click", () => {
-          location.href = "index.html";
+          location.href = "home-account.html";
         });
       });
     }
